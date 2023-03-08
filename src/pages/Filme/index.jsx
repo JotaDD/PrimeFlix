1be73203ad8ea21e6e9b09cd9bbc9fa92a55d196
JotaDD <joaoeduardogomes@gmail.com>
2023-03-08 16:19:33 -0300

    add read-one movie

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import "./filme-info.css"
+
+import api from "../../services/api"
 const Filme = () => {
-  return (
-    <div>
-      <h1>BEM VINDO A PAGINA DETALHES DO FILME</h1>
-    </div>
-  )
+	const { id } = useParams()
+	const [filme, setFilme] = useState({})
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		async function loadFilme() {
+			await api
+				.get(`/movie/${id}`, {
+					params: {
+						api_key: "4aa347f15a417a40d628c0718353997f",
+						language: "pt-br",
+					},
+				})
+				.then((response) => {
+					setFilme(response.data)
+					setLoading(false)
+				})
+				.catch(() => {
+					console.log("Filme não encontrado")
+				})
+		}
+		loadFilme()
+
+		return () => {
+			console.log("Componente foi desmontado")
+		}
+	}, [])
+
+	if (loading) {
+		return (
+			<div className='filme-info'>
+				<h1>Carregando detalhes...</h1>
+			</div>
+		)
+	}
+	return (
+		<div className='filme-info'>
+			<h1>{filme.title}</h1>
+			<img
+				src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+				alt={filme.title}
+			/>
+			<h3>Sinopse</h3>
+			<span>{filme.overview}</span>
+			<strong>Avaliação: {filme.vote_average.toFixed(1)}/10</strong>
+
+			<div className='area-buttons'>
+				<button>Salvar</button>
+				<button><a href="#">Trailer</a></button>
+			</div>
+		</div>
+	)
 }
 
 export default Filme
