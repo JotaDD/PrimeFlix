dd80ee3d12da32dd55d67a5bcfb42c64ef6e81e4
JotaDD <joaoeduardogomes@gmail.com>
2023-03-09 14:05:20 -0300

    add favorite movies to localStorage

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -36,6 +36,22 @@ const Filme = () => {
 		}
 	}, [id, navigate])
 
+	const salvarFilme = () => {
+		const minhaLista = localStorage.getItem("@primeflix")
+
+		let filmesSalvos = JSON.parse(minhaLista) || []
+
+		const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+
+		if (hasFilme) {
+			alert("Esse filme já está na lista")
+			return
+		}
+		filmesSalvos.push(filme)
+		localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+		alert("Filme salvo com sucesso!")
+	}
+
 	if (loading) {
 		return (
 			<div className='filme-info'>
@@ -55,10 +71,10 @@ const Filme = () => {
 			<strong>Avaliação: {filme.vote_average.toFixed(1)}/10</strong>
 
 			<div className='area-buttons'>
-				<button>Salvar</button>
+				<button onClick={salvarFilme}>Salvar</button>
 				<button>
 					<a
-						target='_blank'
+						target='blank'
 						rel='external'
 						href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
 					>
